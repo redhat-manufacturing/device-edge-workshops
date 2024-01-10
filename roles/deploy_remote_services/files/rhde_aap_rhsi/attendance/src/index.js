39607370f35b40613a513d20819cecf465ffdb8f
jeswanso <jeswanso@jeswanso-thinkpadp1gen5.remote.csb>
2024-01-10 14:07:00 -0800

    Updated router

--- roles/deploy_remote_services/files/rhde_aap_rhsi/attendance/src/index.js
+++ roles/deploy_remote_services/files/rhde_aap_rhsi/attendance/src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
